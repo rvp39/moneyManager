608c2da145732d8792244860aaf2b97d0cf826a8
rvp39 <vipulk3009@gmail.com>
2020-06-23 20:13:06 +0530

    functionality added

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs")
+
+var data=[];
 
 app.get("/",function(req,res){
 
-    res.render("page");
+    res.render("page",{data: data});
+});
+
+app.post("/",function(req,res){
+    
+    var Ammount=req.body.Ammount;
+    var Date=req.body.Date;
+    var Comments=req.body.Comments;
+    var temp={Ammount: Ammount,Date: Date,Comments: Comments};
+    data.push(temp);
+    res.redirect("/");
+
 });
 
 app.listen(5000,function(){
